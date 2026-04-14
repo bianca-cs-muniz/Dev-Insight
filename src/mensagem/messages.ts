bffcc7a797b25e559ccc29853605b66ef2740b65
bianca.muniz <biancacaroline2707@hotmail.com>
2026-04-14 00:00:21 -0300

    criando as pasta separdamente

--- src/mensagem/messages.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    ERRO_AO_BUSCAR_USUARIO: 'Erro ao buscar usuário',    
-    ERRO_AO_BUSCAR_REPOSITORIOS: 'Erro ao buscar repositórios',
-    ERRO_AO_COMPARAR_USUARIOS: 'Erro ao comparar usuários',
-    USUARIO_NAO_ENCONTRADO: 'Usuário não encontrado',
-    REPOSITORIOS_NAO_ENCONTRADOS: 'Repositórios não encontrados',
-    USUARIO_OBRIGATORIO: 'Usuário obrigatório',
-    TOKEN_ACABOU: 'Token do git acabou, volte amanhã',
-};
