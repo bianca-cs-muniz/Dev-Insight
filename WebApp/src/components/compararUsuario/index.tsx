a0c6855681513060a05706e059c4a0568cb5d4d6
bianca.muniz <biancacaroline2707@hotmail.com>
2026-04-14 23:05:49 -0300

    criando a tela de comparar os dois desenvolvedores

--- WebApp/src/components/compararUsuario/index.tsx
+++ WebApp/src/components/compararUsuario/index.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Tipografias } from "@shared/components/tipografias";
+import { Cabecalho } from './cabecalho';
+import { Comparar } from './comparar';
 
 export const CompararUsuario = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-8 px-4 py-16">
-      <Tipografias.Titulo>Comparar dois perfis</Tipografias.Titulo>
+    <div className="min-h-screen w-full relative overflow-x-hidden flex flex-col items-center bg-gradient-to-br from-[#f3e8ff] to-[#e0f2fe] selection:bg-purple-200">
+      <Cabecalho />
+      <Comparar />
     </div>
-  )
-}
+  );
+};
