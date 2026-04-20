11f5a5774a138bb3e213be002ee250a9035f5da4
bianca.muniz <biancacaroline2707@hotmail.com>
2026-04-19 23:19:46 -0300

    att css

--- WebApp/src/shared/components/carregandoProcesso/carregandoProcesso.tsx
+++ WebApp/src/shared/components/carregandoProcesso/carregandoProcesso.tsx
@@ -13,7 +13,9 @@ export const CarregandoProcesso = ({ loading, label }: Props) => {
   return (
     <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/60 backdrop-blur-sm animate-in fade-in duration-300">
       <div className="animate-pulse">
-       <Tipografias.Titulo className='!text-white !text-2xl'> {label || 'Carregando...'}</Tipografias.Titulo>
+        <Tipografias.Titulo className='!text-white !text-2xl text-center md:text-left'>
+          {label || 'Carregando...'}
+        </Tipografias.Titulo>
       </div>
     </div>
   );
